30c025a4881b53ccc878f59fb44dc899465c1a8c
SamuelDeboni <samueldeboniss@gmail.com>
2020-11-12 16:26:55 -0300

    Run zig-fmt on the project

--- src/client/main.zig
+++ src/client/main.zig
@@ -8,10 +8,10 @@ const global_allocator = &gpa.allocator;
 
 pub fn main() anyerror!u8 {
     defer _ = gpa.deinit();
-    
+
     try network.init();
     defer network.deinit();
-    
+
     var cli = args.parseForCurrentProcess(CliArgs, global_allocator) catch |err| switch (err) {
         error.EncounteredUnknownArgument => {
             try printUsage(std.io.getStdErr().writer());
@@ -20,17 +20,17 @@ pub fn main() anyerror!u8 {
         else => |e| return e,
     };
     defer cli.deinit();
-    
+
     if (cli.options.help) {
         try printUsage(std.io.getStdOut().writer());
         return 0;
     }
-    
+
     std.log.info("All your codebase are belong to us.", .{});
-    
+
     try draw.init(global_allocator, 800, 600, start, update);
     end();
-    
+
     return 0;
 }
 
@@ -39,13 +39,13 @@ const CliArgs = struct {
     // otherwise use windowed mode
     fullscreen: bool = if (std.builtin.mode != .Debug)
         true
-        else
+    else
         false,
-    
+
     port: u16 = @import("build_options").default_port,
-    
+
     help: bool = false,
-    
+
     pub const shorthands = .{
         .f = "fullscreen",
     };
@@ -53,11 +53,9 @@ const CliArgs = struct {
 
 fn printUsage(writer: anytype) !void {
     try writer.writeAll(
-                        \\
-                        );
+        \\
+    );
 }
-
-
 
 // Potato demo code
 var quad_mesh: draw.Mesh = undefined;
@@ -74,10 +72,10 @@ fn start() void {
     };
     potato = draw.textureFromTgaData(global_allocator, @embedFile("../../assets/potato.tga")) catch unreachable;
     bad_floor = draw.textureFromTgaData(global_allocator, @embedFile("../../assets/bad_floor.tga")) catch unreachable;
-    
+
     cube_mesh.texture = potato;
     quad_mesh = draw.createQuadMesh(global_allocator, 21, 21, 10.5, 10.5, bad_floor, .Tile);
-    
+
     for (quad_mesh.v) |*v| {
         v.pos = rotateVectorOnX(v.pos, 3.1415926535 * 0.5);
         v.pos.y -= 0.5;
@@ -87,7 +85,7 @@ fn start() void {
 fn end() void {
     global_allocator.free(font.texture.raw);
     global_allocator.free(potato.raw);
-    
+
     global_allocator.free(bad_floor.raw);
     global_allocator.free(quad_mesh.v);
     global_allocator.free(quad_mesh.i);
@@ -95,17 +93,16 @@ fn end() void {
 
 var print_buff: [512]u8 = undefined;
 
+var cube_v = [_]Vertex{
+    Vertex.c(Vec3.c(-0.5, 0.5, 0.5), Color.c(0, 0, 0, 1), Vec2.c(0, 1)),
+    Vertex.c(Vec3.c(0.5, 0.5, 0.5), Color.c(0, 0, 1, 1), Vec2.c(1, 1)),
+    Vertex.c(Vec3.c(-0.5, -0.5, 0.5), Color.c(0, 1, 0, 1), Vec2.c(0, 0)),
+    Vertex.c(Vec3.c(0.5, -0.5, 0.5), Color.c(0, 1, 1, 1), Vec2.c(1, 0)),
 
-var cube_v = [_]Vertex {
-    Vertex.c(Vec3.c(-0.5,  0.5,  0.5), Color.c(0, 0, 0, 1), Vec2.c(0, 1)),
-    Vertex.c(Vec3.c( 0.5,  0.5,  0.5), Color.c(0, 0, 1, 1), Vec2.c(1, 1)),
-    Vertex.c(Vec3.c(-0.5, -0.5,  0.5), Color.c(0, 1, 0, 1), Vec2.c(0, 0)),
-    Vertex.c(Vec3.c( 0.5, -0.5,  0.5), Color.c(0, 1, 1, 1), Vec2.c(1, 0)),
-    
-    Vertex.c(Vec3.c(-0.5,  0.5, -0.5), Color.c(1, 0, 0, 1), Vec2.c(1, 1)),
-    Vertex.c(Vec3.c( 0.5,  0.5, -0.5), Color.c(1, 0, 1, 1), Vec2.c(0, 1)),
+    Vertex.c(Vec3.c(-0.5, 0.5, -0.5), Color.c(1, 0, 0, 1), Vec2.c(1, 1)),
+    Vertex.c(Vec3.c(0.5, 0.5, -0.5), Color.c(1, 0, 1, 1), Vec2.c(0, 1)),
     Vertex.c(Vec3.c(-0.5, -0.5, -0.5), Color.c(1, 1, 0, 1), Vec2.c(1, 0)),
-    Vertex.c(Vec3.c( 0.5, -0.5, -0.5), Color.c(1, 1, 1, 1), Vec2.c(0, 0)),
+    Vertex.c(Vec3.c(0.5, -0.5, -0.5), Color.c(1, 1, 1, 1), Vec2.c(0, 0)),
 };
 
 var cube_i = [_]u32{
@@ -132,44 +129,39 @@ var cube_mesh = draw.Mesh{
 usingnamespace draw.vector_math;
 
 fn update(delta: f32) void {
-    
     draw.fillScreenWithRGBColor(50, 100, 150);
-    
+
     // NOTE(Samuel): Mesh Transformation
-    
-    if (draw.keyPressed(.up)) cam.rotation.x += delta * 2; 
+
+    if (draw.keyPressed(.up)) cam.rotation.x += delta * 2;
     if (draw.keyPressed(.down)) cam.rotation.x -= delta * 2;
-    if (draw.keyPressed(.right)) cam.rotation.y += delta * 2; 
+    if (draw.keyPressed(.right)) cam.rotation.y += delta * 2;
     if (draw.keyPressed(.left)) cam.rotation.y -= delta * 2;
-    
+
     var camera_forward = eulerAnglesToDirVector(cam.rotation);
     camera_forward.y = 0;
-    var camera_right = eulerAnglesToDirVector(Vec3.c(cam.rotation.x,
-                                                     cam.rotation.y - 3.1415926535 * 0.5,
-                                                     cam.rotation.z));
+    var camera_right = eulerAnglesToDirVector(Vec3.c(cam.rotation.x, cam.rotation.y - 3.1415926535 * 0.5, cam.rotation.z));
     camera_right.y = 0;
-    
+
     const input_z = draw.keyStrengh(.s) - draw.keyStrengh(.w);
     const input_x = draw.keyStrengh(.d) - draw.keyStrengh(.a);
-    
+
     camera_forward = Vec3_mul_F(camera_forward, input_z);
     camera_right = Vec3_mul_F(camera_right, input_x);
-    
+
     var camera_delta_p = Vec3_add(camera_forward, camera_right);
     camera_delta_p = Vec3_normalize(camera_delta_p);
     camera_delta_p = Vec3_mul_F(camera_delta_p, delta * 3);
-    
+
     cam.pos = Vec3_add(camera_delta_p, cam.pos);
-    
+
     draw.drawMesh(quad_mesh, .Texture, cam);
     draw.drawMesh(cube_mesh, .Texture, cam);
-    
+
     { // Show fps
         const fpst = std.fmt.bufPrint(&print_buff, "{d:0.4}/{d:0.4}", .{ 1 / delta, delta }) catch unreachable;
         draw.drawBitmapFont(fpst, 20, 20, 1, 1, font);
     }
 }
 
-var cam: draw.Camera3D = .{.pos = .{.z = 2.0}};
-
-
+var cam: draw.Camera3D = .{ .pos = .{ .z = 2.0 } };

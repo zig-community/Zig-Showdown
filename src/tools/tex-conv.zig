4ec39f51f46db1ac9932f648c72af3a7ee781b7a
mlarouche <michael.larouche@gmail.com>
2020-12-03 16:35:51 -0500

    Use zigimg instead of stb_image to convert textures in tex-conv tool

--- src/tools/tex-conv.zig
+++ src/tools/tex-conv.zig
@@ -1,10 +1,7 @@
 //! This tool converts Wavefront OBJ to the projects own model format
 const std = @import("std");
 const args = @import("args");
-
-const c = @cImport({
-    @cInclude("stb_image.h");
-});
+const zigimg = @import("zigimg");
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = &gpa.allocator;
@@ -28,20 +25,8 @@ pub fn main() !u8 {
     defer allocator.free(src_data);
 
     // load data:
-    var c_width: c_int = undefined;
-    var c_height: c_int = undefined;
-    const pixel_data = c.stbi_load_from_memory(
-        src_data.ptr,
-        @intCast(c_int, src_data.len),
-        &c_width,
-        &c_height,
-        null,
-        4,
-    ) orelse return error.InvalidFile;
-    defer c.stbi_image_free(pixel_data);
-
-    const width = @intCast(usize, c_width);
-    const height = @intCast(usize, c_height);
+    const image = try zigimg.Image.fromMemory(allocator, src_data[0..]);
+    defer image.deinit();
 
     var tex_file = try std.fs.cwd().createFile(out_file, .{});
     defer tex_file.close();
@@ -58,12 +43,19 @@ pub fn main() !u8 {
     try stream.writeAll("\x99\x5b\x12\x99"); // 0x00: header/identification
     try stream.writeIntLittle(u32, 0x01); // 0x04: version
 
-    try stream.writeIntLittle(u16, @intCast(u16, width)); // 0x08: width
-    try stream.writeIntLittle(u16, @intCast(u16, height)); // 0x0A: width
+    try stream.writeIntLittle(u16, @intCast(u16, image.width)); // 0x08: width
+    try stream.writeIntLittle(u16, @intCast(u16, image.height)); // 0x0A: width
 
     try stream.writeIntLittle(u32, 0x00); // 0x0C: padding
 
-    try stream.writeAll(pixel_data[0 .. 4 * width * height]); // 0x10: data, ([R,G,B,A]*width)*height
+    // 0x10: data, ([R,G,B,A]*width)*height
+    var color_it = image.iterator();
+    while (color_it.next()) |current_color| {
+        const integer_color = current_color.toIntegerColor8();
+
+        const value: u32 = integer_color.R | (@intCast(u32, integer_color.G) << 8) | (@intCast(u32, integer_color.B) << 16) | (@intCast(u32, integer_color.A) << 24);
+        try stream.writeIntLittle(u32, value);
+    }
 
     return 0;
 }

a508cd56c1f40b5df6888afe34ff95048f8b3698
Felix (xq) Queißner <git@mq32.de>
2020-12-04 14:33:03 +0100

    Starts to implement entity-component management.

--- src/client/renderer/implementation/opengl/model.frag
+++ src/client/renderer/implementation/opengl/model.frag
@@ -11,5 +11,11 @@ uniform mat4 uViewProjection;
 uniform sampler2D uAlbedoTexture;
 
 void main() {
-  fragColor = texture(uAlbedoTexture, uv);
+  vec3 ldir = normalize(vec3(0.3, -0.9, 0.2));
+
+  float ldotn = clamp(-dot(ldir, normal), 0.0, 1.0);
+
+  float lighting = 0.3 + 0.8 * ldotn;
+
+  fragColor = vec4(lighting,lighting,lighting,1.0) * texture(uAlbedoTexture, uv);
 }
--- src/client/states/Gameplay.zig
+++ src/client/states/Gameplay.zig
@@ -8,6 +8,9 @@
 
 const std = @import("std");
 const zlm = @import("zlm");
+const log = std.log.scoped(.gameplay);
+
+const components = @import("gameplay/components.zig");
 
 const Self = @This();
 const Game = @import("../Game.zig");
@@ -16,31 +19,118 @@ const Input = @import("../Input.zig");
 const Renderer = @import("../Renderer.zig");
 const Camera = @import("../Camera.zig");
 
+const Entity = enum(u32) {
+    _,
+
+    fn hash(key: Entity) u32 {
+        return @enumToInt(key);
+    }
+    fn eql(a: Entity, b: Entity) bool {
+        return a == b;
+    }
+};
+
+fn OptionalComponentSet(comptime C: type) type {
+    return std.ArrayHashMap(Entity, C, Entity.hash, Entity.eql, false);
+}
+
+fn MandatoryComponentSet(comptime C: type) type {
+    return std.ArrayList(C);
+}
+
 allocator: *std.mem.Allocator,
 resources: *Resources,
 
 level_model_id: Resources.ModelPool.ResourceName,
+healthpack_id: Resources.ModelPool.ResourceName,
 
 cam: Camera = .{
     .position = zlm.vec3(0.0, 0.5, 0.0),
     .euler = zlm.vec3(0, 0, 0),
 },
 
+/// stores the current generation for a given entity index
+entity_generations: std.ArrayList(u16),
+/// stores freed indices in entity_generations
+free_entities: std.ArrayList(u16),
+
+transforms: MandatoryComponentSet(components.Transform),
+point_lights: OptionalComponentSet(components.PointLight),
+static_geometries: OptionalComponentSet(components.StaticGeometry),
+health_packs: OptionalComponentSet(components.HealthPack),
+local_players: OptionalComponentSet(components.LocalPlayer),
+network_players: OptionalComponentSet(components.NetworkPlayer),
+
 pub fn init(allocator: *std.mem.Allocator, resources: *Resources) !Self {
     var self = Self{
         .allocator = allocator,
         .resources = resources,
 
-        .level_model_id = undefined,
-    };
+        .level_model_id = try resources.models.getName("/assets/maps/demo.mdl"),
+        .healthpack_id = try resources.models.getName("/assets/models/healthpack.mdl"),
 
-    self.level_model_id = try self.resources.models.getName("/assets/maps/demo.mdl");
+        .entity_generations = std.ArrayList(u16).init(allocator),
+        .free_entities = std.ArrayList(u16).init(allocator),
+        .transforms = MandatoryComponentSet(components.Transform).init(allocator),
+        .point_lights = OptionalComponentSet(components.PointLight).init(allocator),
+        .static_geometries = OptionalComponentSet(components.StaticGeometry).init(allocator),
+        .health_packs = OptionalComponentSet(components.HealthPack).init(allocator),
+        .local_players = OptionalComponentSet(components.LocalPlayer).init(allocator),
+        .network_players = OptionalComponentSet(components.NetworkPlayer).init(allocator),
+    };
 
     return self;
 }
 
 pub fn deinit(self: *Self) void {
+    self.entity_generations.deinit();
+    self.free_entities.deinit();
+    self.transforms.deinit();
+    self.point_lights.deinit();
+    self.static_geometries.deinit();
+    self.health_packs.deinit();
+    self.local_players.deinit();
+    self.network_players.deinit();
     self.* = undefined;
+}
+
+const EntityData = packed struct {
+    index: u16,
+    generation: u16,
+};
+
+/// Creates a new, unique entity handle
+fn createEntity(self: *Self) !Entity {
+    if (self.free_entities.popOrNull()) |index| {
+        return @bitCast(Entity, EntityData{
+            .index = index,
+            .generation = self.entity_generations.items[index],
+        });
+    }
+    const index = @intCast(u16, self.entity_generations.items.len);
+
+    try self.entity_generations.append(0);
+
+    return @bitCast(Entity, EntityData{
+        .index = index,
+        .generation = 0,
+    });
+}
+
+/// Destroys a previously created entity handle.
+fn destroyEntity(self: *Self, ent: Entity) void {
+    const data = @bitCast(EntityData, ent);
+
+    // check if the entity was already deleted
+    if (self.entity_generations.items[data.index] != data.generation)
+        return;
+
+    self.entity_generations.items[data.index] += 1;
+    self.free_entities.append(data.index) catch |err| {
+        // this operation is not critical when it fails, but
+        // we should log it either.
+        log.emerg("{} in Gameplay.destroyEntity", .{err});
+    };
 }
 
 pub fn update(self: *Self, input: Input, total_time: f32, delta_time: f32) !void {
@@ -81,8 +171,22 @@ pub fn render(self: *Self, renderer: *Renderer, render_target: Renderer.RenderTa
         Resources.usage.level_render,
     );
 
-    // TODO: Replace .cam with an actual mat4
+    const healthpack_model = try self.resources.models.get(
+        self.healthpack_id,
+        Resources.usage.level_render,
+    );
+
     try pass.drawModel(level_model, zlm.Mat4.identity);
+
+    {
+        const pos = zlm.vec3(0, 0.2, 0);
+        const rot = 0.3 * total_time;
+
+        var transform = zlm.Mat4.createAngleAxis(zlm.Vec3.unitY, rot);
+        transform = transform.mul(zlm.Mat4.createTranslation(pos));
+
+        try pass.drawModel(healthpack_model, transform);
+    }
 
     renderer.clear(render_target, Renderer.Color.fromRgb(0.2, 0.4, 0.6));
     try renderer.submit(render_target, pass);
